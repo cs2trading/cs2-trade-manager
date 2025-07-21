cd53b3041c471379ebeffad29c0c354719b2f370
cs2trading <your.email@example.com>
2025-07-21 10:42:27 +0800

    修改插件审核规范

--- src/popup/components/CustomSwitch.vue
+++ src/popup/components/CustomSwitch.vue
@@ -321,8 +321,6 @@ chrome.runtime.onMessage.addListener(async (message, sender, sendResponse) => {
           new Date().getTime(),
         {
           headers: {
-            "User-Agent":
-              "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36",
             Cookie: message.data,
           },
         }
